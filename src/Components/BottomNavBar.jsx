e202ddeb75dc2b09529ed31516674b0176e2b3ed
effyiffurby <iffah.ada@gmail.com>
2024-01-08 17:47:43 +0800

    saving before merging

--- src/Components/BottomNavBar.jsx
+++ src/Components/BottomNavBar.jsx
@@ -10,46 +10,50 @@ import { useState } from "react";
 export default function BottomNavBar({ isLoggedIn }) {
   const [value, setValue] = useState(0);
   return (
-    <Box
-      sx={{
-        position: "fixed",
-        bottom: 0,
-        left: 0,
-        right: 0,
-      }}
-    >
-      <BottomNavigation
-        sx={{ bgcolor: "#4D6D9A" }}
-        showLabels
-        value={value}
-        onChange={(e, newValue) => {
-          setValue(newValue);
-        }}
-      >
-        <BottomNavigationAction
-          sx={{ "*": { color: value === 0 ? "#F78888" : "#90CCF4" } }}
-          component={RouterLink}
-          to="/explore-recommendations"
-          label="Explore"
-          icon={<TravelExploreIcon />}
-        />
+    <div>
+      {isLoggedIn && (
+        <Box
+          sx={{
+            position: "fixed",
+            bottom: 0,
+            left: 0,
+            right: 0,
+          }}
+        >
+          <BottomNavigation
+            sx={{ bgcolor: "#4D6D9A" }}
+            showLabels
+            value={value}
+            onChange={(e, newValue) => {
+              setValue(newValue);
+            }}
+          >
+            <BottomNavigationAction
+              sx={{ "*": { color: value === 0 ? "#F78888" : "#90CCF4" } }}
+              component={RouterLink}
+              to="/explore-recommendations"
+              label="Explore"
+              icon={<TravelExploreIcon />}
+            />
 
-        <BottomNavigationAction
-          sx={{ "*": { color: value === 1 ? "#F78888" : "#90CCF4" } }}
-          component={RouterLink}
-          to="/itinerary"
-          label="Itinerary"
-          icon={<EventNoteIcon />}
-        />
+            <BottomNavigationAction
+              sx={{ "*": { color: value === 1 ? "#F78888" : "#90CCF4" } }}
+              component={RouterLink}
+              to="/itinerary"
+              label="Itinerary"
+              icon={<EventNoteIcon />}
+            />
 
-        <BottomNavigationAction
-          sx={{ "*": { color: value === 2 ? "#F78888" : "#90CCF4" } }}
-          component={RouterLink}
-          to="/"
-          label="Log out"
-          icon={<LogoutIcon />}
-        />
-      </BottomNavigation>
-    </Box>
+            <BottomNavigationAction
+              sx={{ "*": { color: value === 2 ? "#F78888" : "#90CCF4" } }}
+              component={RouterLink}
+              to="/"
+              label="Log out"
+              icon={<LogoutIcon />}
+            />
+          </BottomNavigation>
+        </Box>
+      )}
+    </div>
   );
 }
