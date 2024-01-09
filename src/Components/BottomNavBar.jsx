7852eba010e6589d4e29c39cc2ae026b217b3416
jasperteo <jaspertzj@outlook.sg>
2024-01-09 11:25:34 +0800

    added useEffect to listen for location change and setValue base on location

--- src/Components/BottomNavBar.jsx
+++ src/Components/BottomNavBar.jsx
@@ -4,11 +4,30 @@ import TravelExploreIcon from "@mui/icons-material/TravelExplore";
 import EventNoteIcon from "@mui/icons-material/EventNote";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import Box from "@mui/material/Box";
-import { Link as RouterLink } from "react-router-dom";
-import { useState } from "react";
+import { Link as RouterLink, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function BottomNavBar({ isLoggedIn }) {
   const [value, setValue] = useState(2);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/explore-recommendations":
+        setValue(0);
+        break;
+      case "/itinerary":
+        setValue(1);
+        break;
+      case "/":
+        setValue(2);
+        break;
+      default:
+        setValue(2);
+    }
+  }, [location.pathname]);
+
   return (
     <div>
       {!!isLoggedIn && (
